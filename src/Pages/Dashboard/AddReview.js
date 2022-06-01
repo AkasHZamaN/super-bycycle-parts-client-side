566d2c32abbaf0c94c90447d6c65e48096aa9228
AkasHZamaN <akashzaman4@gmail.com>
2022-06-01 18:24:56 +0600

    review section

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AddReview = () => {
     return (
         <div>
-            <h1>this is review section</h1>
+            <h1 className='flex justify-center items-center text-error text-2xl'>Review Section Upcomming!</h1>
         </div>
     );
 };
